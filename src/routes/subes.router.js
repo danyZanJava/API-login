5ccd22013eee4d8560ee688ecf4432f17803aac2
danyZanJava <daniello63@hotmail.com>
2024-07-08 17:10:26 -0300

    add subesRoutes

--- src/routes/subes.router.js
+++ src/routes/subes.router.js
@@ -2,26 +2,56 @@ const express = require("express");
 const db = require("../db/firebase.js")
 
 const subesRouter = express.Router();
+//-------------------------------------------------------------------------------------------------
+subesRouter.get("/subes", async (req, res) => {
 
-subesRouter.get("/subes", (req, res) => {
+    //buscamos la coleccion Sube
+    const collection = db.collection("Sube");
 
-    res.send("Se obtuvieron las subes");
+    // buscamos los registros/datos en "Sube"
+    const request = await collection.get(); 
+
+    //obtengo los datos de todo el array "Sube" con map()
+    const subes = request.docs.map((elements)=>{
+        return{
+            ...elements.data(),
+            id:elements.id,
+        }
+    })
+    res.send(subes);
+});
+//------------------------------------------------------------------------------------------------
+subesRouter.post("/subes", async(req, res) => {
+
+    // Obtenemos el body
+    const body = res.body;
+
+    // Selecciono la coleccion
+    const collection = db.collection("Sube");
+
+    // Agrego un elemento a la coleccion
+    await collection.add(body);
+
+
+    res.send("Se agrego elemento a la coleccion Sube");
 
 });
+//----------------------------------------------------------------------------------------------
+subesRouter.put("/subes/id", async(req, res) => {
 
-subesRouter.post("/subes", (req, res) => {
-
-    res.send("Se creo una sube");
-
-});
-
-subesRouter.put("/subes", (req, res) => {
-
+    const body = req.body;
+    const id = req.params.id;
+    const collection = db.collection("Subes");
+    await collection.doc(id).update(body);
     res.send("Se actualizo una sube");
 
 });
+//----------------------------------------------------------------------------------------------
+subesRouter.delete("/subes/id",async(req, res) => {
 
-subesRouter.delete("/subes", (req, res) => {
+    const id = req.params.id;
+    const collection = db.collection("Subes");
+    await collection.doc(id).delete();
 
     res.send("Se elimino una sube");
 
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -3,27 +3,48 @@ const db = require("../db/firebase.js")
 
 const usersRouter = express.Router();
 
+//--------CONSULTA/OBTENER DATOS-------------------------------------------------------------------------------------------
+usersRouter.get("/users", async (req, res) => {
 
-usersRouter.get("/users", (req, res) => {
-
-    res.send("Se obtuvieron los usuarios");
+    const collection = db.collection("Users");
+    const request = await collection.get();
+    const users = request.docs.map((elements) =>{
+        return{
+            ...elements.data(),
+           id:elements.id,
+        }
+    })
+    res.send(users);
 
 });
+//----------AGREGAR DATOS-----------------------------------------------------------------------------------------
+usersRouter.post("/users", async (req, res) => {
 
-usersRouter.post("/users", (req, res) => {
+    const body = req.body;
+    const collection = db.collection("Users");
+    await collection.add(body),
 
-    res.send("Se creo un usuario")
+    res.send("Se agrego un usuario")
 
 });
+//---------------ACTUALIZAR DATOS------------------------------------------------------------------------------------
+usersRouter.put("/users/id", async(req, res) => {
 
-usersRouter.put("/users", (req, res) => {
+    const body = req.body;
+    const id = req.params.id;
+    const collection = db.collection("Users");
+    await collection.doc(id).update(body);
 
     res.send("Se actualizo un usuario");
+    //res.send("users[]");
 
 });
+//-------------ELIMINAR DATOS--------------------------------------------------------------------------------------
+usersRouter.delete("/users/id", async(req, res) => {
 
-usersRouter.delete("/users", (req, res) => {
-
+    const id = req.params.id;
+    const collection = db.collection("Users");
+    await collection.doc(id).delete();
     res.send("Se elimino un usuario")
 
 });
